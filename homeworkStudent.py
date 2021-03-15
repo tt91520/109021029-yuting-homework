7ca278f1b3961e35dcedea774a3b8e0661c0120a
tt91520 <109021029@live.asia.edu.tw>
2021-03-15 11:55:19 +0800

    0

--- homeworkStudent.py
+++ homeworkStudent.py
@@ -9,7 +9,6 @@ class Student:
     def showInfo(self):
         print(self.studentName,"\t",self.studentGender,"\t",self.studentHeigh,"\t",self.studentWeight,"\t",self.studentId)
 
-print("姓名", "   性別", "   身高", "  體重", " 座號")
 x = Student("陳大民", "男", "175","60","1")
 x2 = Student("李小明", "男","177","65","2")
 x3 = Student("林阿花", "女","156","52","3")
